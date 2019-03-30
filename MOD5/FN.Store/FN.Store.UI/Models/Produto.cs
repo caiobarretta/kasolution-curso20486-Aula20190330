9ec43cfed3b320e8b83ef711cf3e0b712de60bdd
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-03-30 16:56:21 -0300

    Add files via upload

---
+++ MOD5/FN.Store/FN.Store.UI/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN.Store.UI.Models
+{
+    [Table(nameof(Produto))]
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, Column(TypeName = "varchar(100)")]
+        public string Nome { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Preco { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Tipo { get; set; }
+
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+    }
+}
