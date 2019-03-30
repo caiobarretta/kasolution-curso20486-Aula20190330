167c35c8320588219b7d361f75c59f5d5f366c2b
Caio Augusto Barretta <caio.barretta@gmail.com>
2019-03-30 16:54:31 -0300

    Add files via upload

---
+++ MOD4/FN.Store/FN.Store.UI/Data/DbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using FN.Store.UI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN.Store.UI.Data
+{
+    public static class DbInitializer
+    {
+        public  static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().HasData(
+                    new Produto() { Id = 1, DataCadastro = DateTime.Now, Nome = "Picanha", Preco = 80.5M, Tipo = "Alimento" },
+                    new Produto() { Id = 2, DataCadastro = DateTime.Now, Nome = "Danone", Preco = 9.5M, Tipo = "Alimento" },
+                    new Produto() { Id = 3, DataCadastro = DateTime.Now, Nome = "Fralda Pampers", Preco = 50.5M, Tipo = "Higiene" },
+                    new Produto() { Id = 4, DataCadastro = DateTime.Now, Nome = "Pasta de Dente", Preco = 10.99M, Tipo = "Higiene" }
+                );
+        }
+    }
+}
---
+++ MOD4/FN.Store/FN.Store.UI/Models/SignInVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FN.Store.UI.Models
+{
+    public class SignInVM
+    {
+        [Required]
+        [RegularExpression(@"([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
